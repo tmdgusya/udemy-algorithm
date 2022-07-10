b4772891a22e4c4ee1c00723607699e0166c6053
tmdgusya <tmdgusya@naver.com>
2022-07-10 22:23:47 +0900

    Bubble Sort

---
+++ src/main/java/sort/BubbleSort.java
@@ -0,0 +1,20 @@
+package sort;
+
+public class BubbleSort {
+
+//    private int[] array = {8, 4, 3, 6, 7, 9, 0, 1};
+
+    public int[] bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j+1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+
+}
---
+++ src/test/java/sort/BubbleSortTest.java
@@ -0,0 +1,18 @@
+package sort;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class BubbleSortTest {
+    BubbleSort bubbleSort = new BubbleSort();
+
+    @Test
+    void testBubbleSort() {
+        int[] result = bubbleSort.bubbleSort(new int[]{8, 4, 3, 6, 7, 9, 0, 1});
+        Assertions.assertArrayEquals(new int[]{0, 1, 3, 4, 6, 7, 8, 9}, result);
+    }
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
